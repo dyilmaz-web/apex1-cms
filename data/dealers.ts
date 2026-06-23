5080359506a797d03b34695b8fd0aaead29482f3
unknown <d.yilmaz@cmak.com>
2026-06-23 13:42:36 +0300

    Add dealers data module

---
+++ data/dealers.ts
@@ -0,0 +1,15 @@
+export type Dealer = {
+  id: string
+  name: string
+  city?: string
+}
+
+const dealers: Dealer[] = [
+  {
+    id: "1",
+    name: "Default Dealer",
+    city: "Unknown"
+  }
+]
+
+export default dealers
